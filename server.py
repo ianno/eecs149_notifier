222decc0bdbb6085bacc73a968caec5ffdfdb093
Antonio Iannopollo <antonio@eecs.berkeley.edu>
2014-10-10 14:10:41 -0700

    server python script

---
+++ server.py
@@ -0,0 +1,35 @@
+import urllib2
+import xml.etree.ElementTree as ET
+
+LABEL = '%5Beecs-149-249%5D'
+FILENAME = '149count.txt'
+
+def get_unread_msgs(user, passwd, label = ""):
+    auth_handler = urllib2.HTTPBasicAuthHandler()
+    auth_handler.add_password(
+        realm='New mail feed',
+        uri='https://mail.google.com',
+        user='%s@gmail.com' % user,
+        passwd=passwd)
+    opener = urllib2.build_opener(auth_handler)
+    urllib2.install_opener(opener)
+    feed = urllib2.urlopen('https://mail.google.com/mail/feed/atom/' + label)
+    return feed.read()
+
+def count_unread_emails(xml_text):
+    root = ET.fromstring(xml_text)
+    if root[2] == None:
+        raise NotValidXMLError('count')
+    count = root[2].text
+    return count
+
+def NotValidXMLError(Exception):
+    pass
+
+
+xml_text = get_unread_msgs('name', 'password', LABEL)
+
+print xml_text
+count = count_unread_emails(xml_text)
+
+print count
