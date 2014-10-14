67ce23b52dcd5343b1784a28b984a3305d3b7b99
Antonio Iannopollo <antonio@eecs.berkeley.edu>
2014-10-14 13:54:35 -0700

    code comments and polishing

--- server_script/server.py
+++ server_script/server.py
@@ -2,6 +2,15 @@
 EECS 149/249 Project demo
 Email Notifier - Server
 
+This application constantly polls the gmail RSS atom server to check for unread
+emails with a specific label (in this case [EECS 149/249]).
+The script uses the 'urllib2' library to connect to the server using a secure
+protocol and then write the number of unread emails in a text file.
+
+For the purpose of this demo, the text file is written in a public folder (personal
+ website folder) so that the mbed platform is able to remotely connect to the
+ website and read the uploaded value.
+
 @author: Antonio Iannopollo
 """
 
@@ -9,15 +18,21 @@ import urllib2
 import xml.etree.ElementTree as ET
 import time
 
+#module variables
 GMAIL_USER = 'username'
 GMAIL_PASSWORD = 'password'
-LABEL = 'label'
+LABEL = '%5Beecs-149-249%5D'
 FILEPATH = '/path/to/file'
 
 def get_unread_msgs(user, passwd, label = ""):
+    """
+    This function establishes a secure connection to the Gmail RSS service and
+    returns the XML text associated with the specified Gmail label.
+    Follow the documentation for urllib2 for further details.
+    """
     auth_handler = urllib2.HTTPBasicAuthHandler()
     auth_handler.add_password(
-        realm='New mail feed',
+        realm=None,
         uri='https://mail.google.com',
         user='%s@gmail.com' % user,
         passwd=passwd)
@@ -27,13 +42,24 @@ def get_unread_msgs(user, passwd, label = ""):
     return feed.read()
 
 def count_unread_emails(xml_text):
+    """
+    This method extracts the number of unread emails from the XML string fetched
+    from the Gmail server.
+    We are interested in the 'fullcount' field of the XML file.
+    Since the structure of the XML text is always the same, we can access the
+    field directly with its index (2 from the document root in this case)
+    """
     root = ET.fromstring(xml_text)
     if root[2] == None:
         raise NotValidXMLError('count')
     count = root[2].text
     return count
 
-def NotValidXMLError(Exception):
+class NotValidXMLError(Exception):
+    """
+    An exception to be raised if the structure of the XML text is not what we
+    expect
+    """
     pass
 
 
@@ -42,13 +68,15 @@ if __name__ == '__main__':
     print 'EECS 149/249 email notifier - server'
 
     while True:
-        xml_text = get_unread_msgs(GMAIL_USER, GMAIL_PASSWORD, LABEL)
+#continuosly fetch the number of emails and update the text file, then sleep
+#for 5 seconds
+        XML_TEXT = get_unread_msgs(GMAIL_USER, GMAIL_PASSWORD, LABEL)
 
-        count = count_unread_emails(xml_text)
+        COUNT = count_unread_emails(XML_TEXT)
 
-        file = open(FILEPATH, 'w')
-        file.write(count)
-        file.close()
+        TXT_FILE = open(FILEPATH, 'w')
+        TXT_FILE.write(COUNT)
+        TXT_FILE.close()
 
         time.sleep(5)
 
